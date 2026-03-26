d85fc3485b651285a3b1479a3fda41b798e8206e
tin-blip <tinsaefekadu554@gmail.com>
2026-03-26 11:19:29 +0200

    Create Search in a Binary Search Tree.py

---
+++ Search in a Binary Search Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        while root:
+            if val == root.val:
+                return root
+            elif val < root.val:
+                root = root.left
+            else:
+                root = root.right
+
+        return None
